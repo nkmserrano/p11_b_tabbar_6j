fb4bc957ec4aa57b89e05393029445319ad39913
NAIROBI_KEREN_MONTIEL_SERRANO <a.21308051280973@cbtis128.edu.mx>
2024-02-29 20:09:37 +0000

    Actualizacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const AppTabBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppTabBar extends StatelessWidget {
+  const AppTabBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: 'Ejemplo TabBar',
       theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightBlue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MiPaginaInicial(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MiPaginaInicialState();
+}
+
+class _MiPaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Bolenais',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              )),
+          centerTitle: true,
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                text: 'Ventas',
+                icon: Icon(Icons.local_play_rounded, size: 30.0),
+              ),
+              Tab(
+                  text: 'Artistas',
+                  icon: Icon(Icons.mic_external_on_rounded, size: 30.0)),
+              Tab(
+                  text: 'Festival',
+                  icon: Icon(Icons.calendar_month_outlined, size: 30.0)),
+              Tab(text: 'Pago', icon: Icon(Icons.payment_rounded, size: 30.0)),
+              Tab(
+                  text: 'Sucursales',
+                  icon: Icon(Icons.location_on_sharp, size: 30.0)),
+            ], //Texto Icono
+          ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_play_rounded,
+                size: 300.0, color: Color(0xFFFCB4335)),
+            Icon(Icons.mic_external_on_rounded,
+                size: 300.0, color: Color(0xFFF2E4053)),
+            Icon(Icons.calendar_month_outlined,
+                size: 300.0, color: Color(0xFFFA04000)),
+            Icon(Icons.payment_rounded, size: 300.0, color: Color(0xFFF2471A3)),
+            Icon(Icons.location_on_sharp,
+                size: 300.0, color: Color(0xFFF28B463))
+          ],
         ),
       ),
     );
